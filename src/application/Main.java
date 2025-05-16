2847f39a31280f81282db6c9bcc41e79e3ddede5
Mohamed Shaffaq <mohamedshaffaq@gmail.com>
2025-05-16 11:12:45 +0530

    Testing the JavaFX code

--- src/application/Main.java
+++ src/application/Main.java
@@ -2,26 +2,35 @@ package application;
 	
 import javafx.application.Application;
 import javafx.stage.Stage;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.paint.Color;
 
 
 public class Main extends Application {
-	@Override
-	public void start(Stage primaryStage) {
-		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-			
-		}
-	}
+
 	
 	public static void main(String[] args) {
 		launch(args);
+	}
+
+	@Override
+	public void start(Stage stage) throws Exception {
+		// TODO Auto-generated method stub
+		Group root = new Group();
+		
+		Scene scene = new Scene(root, Color.AZURE);
+		
+		stage.setTitle("javaFX project");
+		
+		stage.setWidth(420);
+		stage.setHeight(420);
+
+		stage.setFullScreen(true);
+		stage.setFullScreenExitHint("You cannot escape unless you press exit");
+		
+		stage.setScene(scene);
+		stage.show();
 	}
 }
